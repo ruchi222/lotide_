ea6bfe7b86318ed05959c4f58cd8741e7289b15a
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-07-20 15:31:53 -0400

    Update dice-roller.js

--- dice-roller.js
+++ dice-roller.js
@@ -1,7 +1,15 @@
-function diceroller(num) {
-	for (let i = 0; i <= num - 1;  i++) {
-		let ranDice = Math.floor(6 * Math.random()) + 1;
-		console.log(ranDice)
-	};
+function diceroller() {
+    const number = process.argv[2]
+    let output = "";
+	for (let i = 0; i < number; i++) {
+        const randomNumber = Math.floor(6 * Math.random());
+		if(i === number-1) {
+            output += randomNumber;
+        } else {
+            output += randomNumber + ', '
+        }
+    };
+
+    console.log(`Rolled ${number}: ${output} `)
 };
-diceroller(5);
+diceroller();
