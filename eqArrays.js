a880bdb696a7756d53e80bae703d53a3d7099e49
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-20 18:15:44 -0400

    Implemented the eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+  
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
