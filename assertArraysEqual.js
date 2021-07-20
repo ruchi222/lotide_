b4e28b2c0355cc28f95cd1df84fd7229616b7504
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-20 18:43:23 -0400

    Compared the two arrays in this function

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const assertArraysEqual = function (array1,array2) {
+    if (eqArrays(array1, array2)) {
+        console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+    } else {
+      console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+    }
+}
+
+
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+// TEST CODE
+assertArraysEqual("Lighthouse Labs", "Lighthouse Labs");
+assertArraysEqual([1, 5], [1, 5]);
