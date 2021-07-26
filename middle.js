a6a3b27d5c0f3de09fa7d7ce58711154685a1c7b
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-26 14:42:49 -0400

    Implemented the function

--- middle.js
+++ middle.js
@@ -1,24 +1,24 @@
-const assertArraysEqual = function (array1,array2) {
-    if (eqArrays(array1, array2)) {
-        console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
-    }
-}
+// const assertArraysEqual = function (array1,array2) {
+//     if (eqArrays(array1, array2)) {
+//         console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+//     } else {
+//       console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+//     }
+// }
 
 
-const eqArrays = function(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    } else {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
+// const eqArrays = function(arr1, arr2) {
+//     if (arr1.length !== arr2.length) {
+//         return false;
+//     } else {
+//         for (let i = 0; i < arr1.length; i++) {
+//             if (arr1[i] !== arr2[i]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// };
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -35,15 +35,4 @@ const middle = function(array) {
         
 }
 
-
-middle([1]) // => []
-middle([1, 2]) // => []
-
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
+module.exports = middle;
