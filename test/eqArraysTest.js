ea258cee2dd3a0a870f0a35a76555268e5655644
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-26 14:18:07 -0400

    import the logic

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+  
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
